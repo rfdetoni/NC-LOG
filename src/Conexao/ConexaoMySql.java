ffb493b994fef323563a1660fc7225757247b725
Mafra <luizm14.tk@gmail.com>
2021-06-15 20:38:53 -0300

    teste de commit

--- src/Conexao/ConexaoMySql.java
+++ src/Conexao/ConexaoMySql.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConexaoMySql {
-
+    // teste commit
     public String status = "Nao conectou...";
     public Connection connection = null;
 
