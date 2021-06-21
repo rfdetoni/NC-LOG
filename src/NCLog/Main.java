8ca2a8796c3e9175e9d5071be51c20f24292585d
Mafra <luizm14.tk@gmail.com>
2021-06-21 17:46:40 -0300

    Importei a mainscreen para a nossa classe Main() do java.

--- src/NCLog/Main.java
+++ src/NCLog/Main.java
@@ -1,11 +1,11 @@
 package NCLog;
 
-
+import visual.MainScreen;
 
 public class Main {
     public static void main(String[] args) {
-
-        
+    	MainScreen ms = new MainScreen();
+    	ms.setVisible(true);
   
     }
     
