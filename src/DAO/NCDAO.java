e955f911714b6882fd10bc66352a3612bd5678f3
rfdetoni <85571449+rfdetoni@users.noreply.github.com>
2021-06-22 11:46:16 -0300

    full

---
+++ src/DAO/NCDAO.java
@@ -0,0 +1,104 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import DTO.NcDTO;
+import DTO.UsuarioDTO;
+import visual.okNext;
+
+public class NCDAO {
+	Connection conn;
+	PreparedStatement pstm;
+	ResultSet rs;
+	ArrayList<NcDTO> lista =  new ArrayList<>();
+	private static String resp_nc;
+	private String pesquisa;
+	
+	
+	
+	public void registerNC(NcDTO objnc) {
+
+		String sql = "insert into naoConformidade (nome_Nc,descricao_Nc,local_Nc,responsavel_Nc) values (?,?,?,?)";
+		conn = new ConexaoDAO().conectaDB();
+		try {
+
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, objnc.getNome_Nc());
+			pstm.setString(2, objnc.getDescricao_Nc());
+			pstm.setString(3, objnc.getLocal_Nc());
+			pstm.setString(4, objnc.getResponsavel_Nc());
+
+			pstm.execute();
+			pstm.close();
+			okNext ok = new okNext();
+			ok.setVisible(true);
+
+		} catch (Exception erro) {
+			JOptionPane.showMessageDialog(null, "NCDAO - registerNC " + erro);
+		}
+
+	}
+	
+	public void setPesquisa(String pesquisa) {
+		this.pesquisa = pesquisa;
+		
+	}
+	public String getPesquisa() {
+		return pesquisa;
+	}
+	
+	
+	public void setRespNc(String resp_nc) {
+		resp_nc = resp_nc;
+		
+	}
+	
+	public String getRespNc() {
+		return resp_nc;
+	}
+	
+	
+	public	ArrayList<NcDTO> pesquisarNC(String pesquisa){
+		
+	
+		
+		
+		
+		try {
+			
+			String sql = "select * from naoConformidade where responsavel_Nc = " +"'"+ this.getRespNc() +"'"+ " and nome_Nc = "+"'"+ pesquisa +"'";
+			// JOptionPane.showMessageDialog(null, sql); debug da pesquisa
+			conn = new ConexaoDAO().conectaDB();
+			
+			pstm = conn.prepareStatement(sql);
+			rs = pstm.executeQuery();
+			
+			
+			while (rs.next()){
+				NcDTO objnc = new NcDTO();
+				objnc.setId_Nc(rs.getInt("id_Nc"));
+				objnc.setDescricao_Nc(rs.getString("descricao_Nc"));
+				objnc.setLocal_Nc(rs.getString("local_Nc"));
+				objnc.setNome_Nc(rs.getString("nome_Nc"));
+				objnc.setResponsavel_Nc(rs.getString("responsavel_NC"));
+				objnc.setId_Plano(rs.getInt("id_Plano"));
+				
+				lista.add(objnc);
+
+			}
+			
+			
+			
+			
+		} catch (SQLException erro) {
+			JOptionPane.showMessageDialog(null, "NCDAO - pesquisarNC " + erro);
+		}return lista;
+	}
+
+}
